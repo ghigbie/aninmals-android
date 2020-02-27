eef2a7f1fef22b5ba3f4e57ff557d6d61d93e314
Derfel Cadarn <gehigbie@gmail.com>
2020-02-26 21:08:04 -0700

    Edit layout

--- app/src/main/java/com/traiingtrack/animals/view/MainActivity.kt
+++ app/src/main/java/com/traiingtrack/animals/view/MainActivity.kt
@@ -8,12 +8,10 @@ import com.traiingtrack.animals.R
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        navController = Navigation.findNavController(this, R.id.fragment)
     }
 }
