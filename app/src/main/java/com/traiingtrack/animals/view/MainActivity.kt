f6ff3a73346ff6f73e214b7c668cff639b1de854
Derfel Cadarn <gehigbie@gmail.com>
2020-02-26 20:46:10 -0700

    Add nav host fragment

--- app/src/main/java/com/traiingtrack/animals/view/MainActivity.kt
+++ app/src/main/java/com/traiingtrack/animals/view/MainActivity.kt
@@ -2,12 +2,18 @@ package com.traiingtrack.animals.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
 import com.traiingtrack.animals.R
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        navController = Navigation.findNavController(this, R.id.fragment)
     }
 }
