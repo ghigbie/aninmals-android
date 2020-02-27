487053914092033ffcb47ce800655f922c7e9761
Derfel Cadarn <gehigbie@gmail.com>
2020-02-26 20:21:31 -0700

    Edit ids of buttons

--- app/src/main/java/com/traiingtrack/animals/view/ListFragment.kt
+++ app/src/main/java/com/traiingtrack/animals/view/ListFragment.kt
@@ -22,5 +22,10 @@ class ListFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_list, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+    }
 
 }
