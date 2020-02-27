3aa054e024aa92937ad7ca94ae740102af0a69d8
Derfel Cadarn <gehigbie@gmail.com>
2020-02-26 19:57:10 -0700

    Add new list fragment

--- app/src/main/java/com/traiingtrack/animals/view/MainActivity.kt
+++ app/src/main/java/com/traiingtrack/animals/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.traiingtrack.animals
+package com.traiingtrack.animals.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.traiingtrack.animals.R
 
 class MainActivity : AppCompatActivity() {
 
