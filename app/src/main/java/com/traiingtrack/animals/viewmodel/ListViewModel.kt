d0e5351a91d368b6d62aa43bbdcaf3e4d177261a
ghigbie <gehigbie@gmail.com>
2020-02-29 19:39:55 -0700

    Update view model

--- app/src/main/java/com/traiingtrack/animals/viewmodel/ListViewModel.kt
+++ app/src/main/java/com/traiingtrack/animals/viewmodel/ListViewModel.kt
@@ -1,9 +1,11 @@
 package com.traiingtrack.animals.viewmodel
 
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
 import com.traiingtrack.animals.model.Animal
 
-class ListViewModel {
+class ListViewModel(application: Application): AndroidViewModel(application){
     val animals by lazy { MutableLiveData<List<Animal>>() }
     val loadError by lazy { MutableLiveData<Boolean>() }
     val Laoding by lazy { MutableLiveData<Boolean>() }
