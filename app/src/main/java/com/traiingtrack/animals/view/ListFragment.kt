b72c82e7d2827135d7eeedf6ec655a20236be7a5
ghigbie <gehigbie@gmail.com>
2020-02-29 21:28:28 -0700

    Set up observers

--- app/src/main/java/com/traiingtrack/animals/view/ListFragment.kt
+++ app/src/main/java/com/traiingtrack/animals/view/ListFragment.kt
@@ -6,11 +6,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.NavDirections
 import androidx.navigation.Navigation
+import androidx.recyclerview.widget.GridLayoutManager
 
 import com.traiingtrack.animals.R
+import com.traiingtrack.animals.model.Animal
+import com.traiingtrack.animals.view.AnimalListAdapater
 import com.traiingtrack.animals.view.ListFragmentDirections
+import com.traiingtrack.animals.viewmodel.ListViewModel
 import kotlinx.android.synthetic.main.fragment_list.*
 
 
@@ -18,6 +25,22 @@ import kotlinx.android.synthetic.main.fragment_list.*
  * A simple [Fragment] subclass.
  */
 class ListFragment : Fragment() {
+
+    private lateinit var viewModel: ListViewModel
+    private val listAdapater = AnimalListAdapater(arrayListOf())
+    private val animalListDataObserver = Observer<List<Animal>> {list ->
+
+    }
+
+    private val loadingLiveDataObserver = Observer<Boolean>{isLoading ->
+
+    }
+    
+    private val errorLiveDataObserver = Observer<Boolean> {isError ->
+
+    }
+
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -29,12 +52,18 @@ class ListFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        detailButton.setOnClickListener {
-            val action: NavDirections =
-                ListFragmentDirections.actionDetail()
-            Navigation.findNavController(it).navigate(action)
 
+        viewModel = ViewModelProviders.of(this).get(ListViewModel::class.java)
+        viewModel.animals.observe(this, animalListDataObserver)
+        viewModel.loading.observe(this, loadingLiveDataObserver)
+        viewModel.loadError.observe(this, errorLiveDataObserver)
+        viewModel.refresh()
+
+        animalList.apply {
+            layoutManager = GridLayoutManager(context, 2)
         }
     }
+
+
 
 }
