cb4d83295d43354431dfd5c8ddcd6233b7717bd3
ghigbie <gehigbie@gmail.com>
2020-03-06 20:59:29 -0700

    Edit animal class

--- app/src/main/java/com/traiingtrack/animals/model/Animal.kt
+++ app/src/main/java/com/traiingtrack/animals/model/Animal.kt
@@ -1,5 +1,32 @@
 package com.traiingtrack.animals.model
 
+import com.google.gson.annotations.SerializedName
+
+data class ApiKey(
+    val message: String?,
+    val key: String?
+)
+
 data class Animal(
-    val name: String?
+    val name: String?,
+    val taxonmy: Taxonmy?,
+    val location: String,
+    val speed: Speed?,
+
+    @SerializedName("lifespan")
+    val lifeSpan: String?,
+
+    @SerializedName("image")
+    val imageUrl: String?
+)
+
+data class Taxonmy(
+    val kingdom: String?,
+    val name: String?,
+    val family: String?
+)
+
+data class Speed(
+    val metric: String?,
+    val imperial: String?
 )
